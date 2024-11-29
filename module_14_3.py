f3b08c740d15b538374cfc46ce2f667f7307933f
Алина Демчук <alina2805@bk.ru>
2024-11-29 17:15:57 +0300

    Add files via upload

--- module_14_3.py
+++ module_14_3.py
@@ -5,14 +5,22 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMar
 
 
 
-api = '7539261653:AAE1xDsK4vPWU1fxV9GzSf5xd6Mz6A91fU8'
+api = ''
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
 menu = ReplyKeyboardMarkup([
-    [KeyboardButton(text='Информация'),KeyboardButton(text='Купить')]
+    [KeyboardButton(text='Информация'), KeyboardButton(text='Рассчитать'),
+        KeyboardButton(text='Купить')]
 ],
 resize_keyboard=True)
+
+inline_choices = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text='Рассчитать норму калорий', callback_data='calories'),
+         InlineKeyboardButton(text='Формулы расчёта', callback_data='formulas')]
+    ]
+)
 
 inline_products = InlineKeyboardMarkup(
     inline_keyboard=[
@@ -33,9 +41,9 @@ class UserState(StatesGroup):
 @dp.message_handler(text="Купить")
 async def get_buying_list(message):
     for i in range(1, 5):
-        await message.answer(f'Название: {i} | Описание: описание {i} | Цена: {i*100}' )
-        print(f'Название: {i} | Описание: описание {i} | Цена: {i*100}' )
-        with open(f'image/image{i}.jpg.jpg', 'rb') as photo:
+        await message.answer(f'Название: {i} | Описание: описание {i} | Цена: {i*100}')
+        print(f'Название: {i} | Описание: описание {i} | Цена: {i*100}')
+        with open(f'image/image{i}.jpg', 'rb') as photo:
             await  message.answer_photo(photo)
             print(f'Отправлено изображение {i}')
     await message.answer("Выберите продукт для покупки:", reply_markup=inline_products)
@@ -51,6 +59,46 @@ async def send_confirm_message(call):
 async def main_menu(message):
     await message.answer('Выберите опцию:', reply_markup=inline_choices)
     print('Выберите опцию:')
+
+@dp.callback_query_handler(text='formulas')
+async def get_formulas(call):
+    await call.message.answer('Формулы расчета: 10 x вес (кг) + 6,25 x рост (см) – 5 x возраст (г) – 161')
+    print('Формулы расчета: 10 x вес (кг) + 6,25 x рост (см) – 5 x возраст (г) – 161')
+
+
+@dp.callback_query_handler(text='calories')
+async def set_age(call):
+    await call.message.answer('Введите свой возраст:')
+    print('Введите свой возраст:')
+    await UserState.age.set()
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message, state):
+    await state.update_data(age=message.text)
+    await message.answer('Введите свой рост:')
+    print('Введите свой рост:')
+    await UserState.growth.set()
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(growth=message.text)
+    await message.answer('Введите свой вес:')
+    print('Введите свой вес:')
+    await UserState.weight.set()
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message, state):
+    await state.update_data(weight=message.text)
+    data = await state.get_data()
+    result = 10 * int(data['weight']) + 6.25 * int(data['growth']) - 5 * int(data['age']) - 161
+    await message.answer(f'Ваша норма калорий составляет: {result}')
+    print(f'Ваша норма калорий составляет: {result}')
+    await state.finish()
+
+    # для мужчин: 10 х вес (кг) + 6,25 x рост (см) – 5 х возраст (г) + 5;
+    # для женщин: 10 x вес (кг) + 6,25 x рост (см) – 5 x возраст (г) – 161.
+
+
 
 
 @dp.message_handler(commands='start')
